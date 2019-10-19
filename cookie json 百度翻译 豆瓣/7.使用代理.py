7f1e6894fd44e27cecb0ada762131b18d656cf42
bauble <55495488+bwyt@users.noreply.github.com>
2019-10-19 15:13:01 +0800

    Update 7.使用代理.py

--- cookie json 百度翻译 豆瓣/7.使用代理.py
+++ cookie json 百度翻译 豆瓣/7.使用代理.py
@@ -1,20 +1,31 @@
-import random
+"""
+使用代理：让服务器认为是不同的客户端在发送请求
+	     防止我们的真实地址被泄露,防止被追究责任
+         
+         正向代理保护客户端
+	     反向代理保护服务器
+         
+代理分类
+		透明代理:知道真实IP知道使用代理
+		匿名代理:知道使用代理但是不知道真实IP
+		高匿代理:既不知道使用代理有不知道真实IP
+"""
 
+import random
 import requests
 
-# 1.准备代理字典 --> 最多两组键值对 --> http/https
-# 键：http/https
-# 值：键://账号:密码@IP:post（端口）
-# p = {
-#     'http': '223.111.131.100:8080',
-#     # 'https': ''
-# }
-#
-# url = 'http://httpbin.org/get'
-#
-# response = requests.get(url, proxies=p)
-
-# print(response.content.decode())
+"""
+准备代理字典 --> 最多两组键值对 --> http/https
+键：http/https
+值：键://账号:密码@IP:post（端口）
+"""
+p = {
+    'http': '223.111.131.100:8080',
+    # 'https': ''
+}
+url = 'http://httpbin.org/get'
+response = requests.get(url, proxies=p)
+print(response.content.decode())
 
 
 # 随机代理：准备一个代理列表，每一个元素就是一个字典
@@ -27,10 +38,9 @@ p_list = [
 ]
 url = 'http://httpbin.org/get'
 for i in range(1, 10):
-    p = random.choice(p_list)
+    p = random.choice(p_list)  # 随机取一个代理IP
     try:
         response = requests.get(url, proxies=p, timeout=3)
-        print(response.content.decode())
+        print(response.content.decode())  # 使用代理IP发送请求
     except Exception as e:
         print('不可使用的代理有：{}'.format(p))
-

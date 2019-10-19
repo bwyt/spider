b61abdffe0e4e9d41688cd9bca9214a7d5510fe6
bauble <55495488+bwyt@users.noreply.github.com>
2019-10-19 14:49:49 +0800

    cookirJar的使用

--- cookie json 百度翻译 豆瓣/2.cookiejar.py
+++ cookie json 百度翻译 豆瓣/2.cookiejar.py
@@ -1,12 +1,10 @@
 # 将cookiejar转换成字典
 import requests
-
 response = requests.get('http://www.baidu.com')
-
 cookiejar = response.cookies
 
-dic = requests.utils.dict_from_cookiejar(cookiejar)
+cookie_dic = requests.utils.dict_from_cookiejar(cookiejar)
+cookie_jar = requests.utils.cookiejar_from_dict(cookie_dic)
 
 print(dic)
-cookie = requests.utils.cookiejar_from_dict(dic)
 print(cookie)

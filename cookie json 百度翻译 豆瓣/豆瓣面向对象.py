fcbdb0e78a274c843d858d85082e453b5e2fb93c
bauble <55495488+bwyt@users.noreply.github.com>
2019-10-19 16:05:16 +0800

    面向对象修改

--- cookie json 百度翻译 豆瓣/豆瓣面向对象.py
+++ cookie json 百度翻译 豆瓣/豆瓣面向对象.py
@@ -1,19 +1,11 @@
-"""
-实例化
-获取url列表
-获取数据
-保存数据
-run发放
-"""
-
 import requests,json
-
-
-class Me:
+class Douban:
+    # 实例化
     def __init__(self):
         self.h = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
         self.url = 'https://movie.douban.com/j/search_subjects?type=tv&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=200&page_start=0'
-
+    
+    # 获取数据
     def get_data(self):
         response = requests.get(self.url,headers=self.h)
         data = response.json()
@@ -29,15 +21,16 @@ class Me:
             dic['rate'] = i['rate']
             dic['cover'] = i['cover']
             data_list.append(dic)
-
         return data_list
-
+    
+    # 保存数据
     def save_data(self, data_list):
-        with open('豆瓣11.json', 'w', encoding='utf-8') as f:
+        with open('豆瓣.json', 'w', encoding='utf-8') as f:
             for data in data_list:
                 json.dump(data, f, ensure_ascii=False)
                 f.write(',\n')
 
+    # run发放
     def run(self):
         list = self.get_data()
         data_list = self.pre_dic(list)
@@ -45,5 +38,5 @@ class Me:
 
 
 if __name__ == '__main__':
-    p = Me()
-    p.run()
+    d = Douban()
+    d.run()

322056291d9bdfd5cd77213b3f27f364431fe103
bauble <55495488+bwyt@users.noreply.github.com>
2019-10-22 21:39:43 +0800

    获取元素和数据

--- selenium/6.selenium获取值.py
+++ selenium/6.selenium获取值.py
@@ -7,11 +7,10 @@ dirver.get('https://www.baidu.com')
 
 ass = dirver.find_element_by_link_text('hao123')
 # 标签属性的值
-# print(ass.get_attribute('href'))
-# print(type(ass.get_attribute('href')))
+print(ass.get_attribute('href'))
+
 # # 获取文本信息
-# print(ass.text)
-# print(type(ass.text))
+print(ass.text)
 
 # 先分组在获取数据
 div = dirver.find_element_by_id('u1')
@@ -24,26 +23,10 @@ for a in a_s:
 time.sleep(3)
 dirver.close()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
+get_attribute(key) : 获取节点上的属性
+text: 获取该节点上以及子节点的文本
+click(): 点击操作
+send_keys(): 向输入框中输入内容.
+"""
 

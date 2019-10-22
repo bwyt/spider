5a1c0611c8befb3d4b645129eab12da4095e5992
bauble <55495488+bwyt@users.noreply.github.com>
2019-10-22 21:52:13 +0800

    frame切换和窗口切换

--- selenium/11. selenium切换frame.py
+++ selenium/11. selenium切换frame.py
@@ -1,73 +1,23 @@
 import time
-
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-
 driver.get('https://mail.qq.com/cgi-bin/loginpage')
 time.sleep(2)
+
+# frame切换 --> switch_to.frame('name属性值')
 driver.switch_to.frame('login_frame')
+
+"""
+切换window窗口
+		driver.window_handles -->  获取driver中所有窗口的名称
+		driver.switch_to.window(窗口名称)  -->  把当前driver切换到指定窗口
+"""
+
+# send_keys()  -->  向输入框中输入内容
 driver.find_element_by_id('u').send_keys('502046651')
 
 time.sleep(3)
 driver.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 

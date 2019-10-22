f4a0e088b231e3cba3517980e5b691de70f513c4
bauble <55495488+bwyt@users.noreply.github.com>
2019-10-22 21:36:07 +0800

    定位页面元素

--- selenium/5. selenium定位元素.py
+++ selenium/5. selenium定位元素.py
@@ -1,30 +1,34 @@
 import time
-
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-
 driver.get('https://www.baidu.com')
 
-# 1. 通过id定位标签
-# driver.find_element_by_id('kw').send_keys("python")
-# 2. 通过类属性值定位标签
-# driver.find_element_by_class_name('s_ipt').send_keys('haha')
-# 3. 通过标签name属性的值进行定位
-# driver.find_element_by_name("wd").send_keys('hehe')
-# 4. 通过a标签的文本内容定义标签
-# by_link_text 文本内容必须完全一致
-# driver.find_element_by_link_text('hao123').click()
-# by_partial_link_text 文本内容可以部分一致
-# driver.find_element_by_partial_link_text('hao').click()
+"""
+定位页面元素:find_element_by_XXX --> element对象
 
-# inp = driver.find_element_by_id('kw')
-# find_element_by_XXX 会返回一个element对象
-#  5. 通过标签名去获取标签
-# ass = driver.find_element_by_tag_name('a')
-# print(ass)
-# 6. 通过xpath去定位
-# driver.find_element_by_xpath('//a[@name="tj_trnews"]').click()
+    通过id定位标签                                  driver.find_element_by_id('kw').send_keys("python")
+    通过class属性值定位标签                          driver.find_element_by_class_name('s_ipt').send_keys('haha')
+    通过标签name属性的值进行定位                     driver.find_element_by_name("wd").send_keys('hehe')
+   
+    通过a标签的文本内容定义标签：
+        by_link_text 文本内容必须完全一致            driver.find_element_by_link_text('hao123').click()
+        by_partial_link_text 文本内容可以部分一致    driver.find_element_by_partial_link_text('hao').click()
+    
+    通过标签名去获取标签                             ass = driver.find_element_by_tag_name('a')
+    通过xpath去定位                                 driver.find_element_by_xpath('//a[@name="tj_trnews"]').click()
+    
+    
+注意点:
+    find_element 和find_elements的区别：
+				find_element: 返回找到第一个元素,如果没有报错
+				find_elements: 返回找到的所有元素列表, 如果没有找到返回空列表
+    by_link_text和by_partial_link_text的区别：
+				by_link_text: 全部文本都一样
+				by_partial_link_text: 包含某个文本
+		find_element/ find_elements只能获取元素, 不能获取属性和文本, 要获取属性和文本需要使用get_attribute(属性名) 和.text
+
+"""
 
 # 返回一个element对象的列表
 ass = driver.find_elements_by_tag_name('a')

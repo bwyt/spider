1e46d07e44c5e994a4bab2dbb894fb2a09da872c
bauble <55495488+bwyt@users.noreply.github.com>
2019-10-22 22:02:05 +0800

    下拉滚动条

--- selenium/12. selenium最大化窗口和下拉滚动条.py
+++ selenium/12. selenium最大化窗口和下拉滚动条.py
@@ -1,74 +1,16 @@
 import time
-
 from selenium import webdriver
 
 driver = webdriver.Chrome()
+
+# 窗口最大化
 driver.maximize_window()
 
 driver.get('https://www.baidu.com/s?wd=python')
 time.sleep(2)
+# 下拉滚动条 -->  execute_script('window.scrollTo(横,纵)')
 driver.execute_script('window.scrollTo(0,800)')
-
 
 time.sleep(3)
 driver.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
